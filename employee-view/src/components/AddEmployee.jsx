6f7f7dea23f9d149ebfc5f9b3b63bfbde65b5b33
Mahamadtohid <naikwadimdtohid@gmail.com>
2025-04-14 12:51:24 +0530

    Connection with Spring Boot

--- employee-view/src/components/AddEmployee.jsx
+++ employee-view/src/components/AddEmployee.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import EmployeeService from '../service/EmployeeService'
+
 
 function AddEmployee() {
-    const navigate = useNavigate();
+
+  const [employee, setEmployee] = useState({
+    name: "",
+    email: "",
+    phone: ""
+  });
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setEmployee({...employee, [e.target.name]: value});
+
+  }
+    
+
+    const reset = (e) => {
+      e.preventDefault();
+      setEmployee({
+        name: "",
+        email: "",
+        phone: ""
+      });
+    }
+
+    const saveEmployee = (e) => {
+      e.preventDefault();
+      EmployeeService.saveEmployee(employee)
+      .then((response)=>{
+        console.log("Data saved Successfully",response);
+        navigate('/');
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+
+    }
+
+  const navigate = useNavigate();
   return (
     <div className='max-w-xl mx-40 bg-slate-800 my-20 rounded shadow py-4 px-8'>
         <div className="text-4xl tracking-wider font-bold text-center py-4 px-8">
@@ -11,14 +50,21 @@ function AddEmployee() {
 
         <div className='mx-10 my-2'>
         {/* <h1>AddEmployee</h1> */}
-        <input className='bg-white w-full py-2 my-4 text-slate-800' name="name" type="text" placeholder='Name'></input>
-        <input className='bg-white w-full py-2 my-4 text-slate-800' name="email" type="email" placeholder='Email'></input>
-        <input  className='bg-white w-full py-2 my-4 text-slate-800' name="phone" type="number" placeholder='Phone'></input>
+        <input className='bg-white w-full py-2 my-4 text-slate-800' value ={employee.name} onChange={(e) => handleChange(e)} name="name" type="text" placeholder='Name'></input>
+        <input className='bg-white w-full py-2 my-4 text-slate-800' value ={employee.email} onChange={(e) => handleChange(e)}name="email" type="email" placeholder='Email'></input>
+        <input  className='bg-white w-full py-2 my-4 text-slate-800' value ={employee.phone} onChange={(e) => handleChange(e)} name="phone" type="number" placeholder='Phone'></input>
         </div>
 
         <div className='flex px-20 space-x-4 my-4'>
-        <button className='bg-green-400 hover:bg-green-700 py-2 px-6 rounded'>Save</button>
-        <button className='bg-blue-400 hover:bg-blue-700 py-2 px-6 rounded'>Clear</button>
+        <button
+        
+        onClick = {saveEmployee}
+        className='bg-green-400 hover:bg-green-700 py-2 px-6 rounded'>Save</button>
+        
+        <button 
+        onClick={reset}
+        
+        className='bg-blue-400 hover:bg-blue-700 py-2 px-6 rounded'>Clear</button>
         <button
         onClick={() => navigate('/')}
 
